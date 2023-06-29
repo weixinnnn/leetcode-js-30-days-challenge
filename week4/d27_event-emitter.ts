c7f5c66550765099d4ea9f24c25bf843b745f5a3
weixinnnn <weixinwxo@gmail.com>
2023-06-29 11:31:01 +0800

    Add examples

--- week4/d27_event-emitter.ts
+++ week4/d27_event-emitter.ts
@@ -31,3 +31,15 @@ class EventEmitter {
     return this.events[event].map((f) => f(...args));
   }
 }
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
